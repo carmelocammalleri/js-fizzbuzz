0801134067339d3de29b6a010085ea442b14031c
Melozzo <carmecamma1995@gmail.com>
2023-09-11 16:53:45 +0200

    [fix]: inaccuracies

--- js/script.js
+++ js/script.js
@@ -4,22 +4,20 @@ for(let i = 1; i <= 100; i++){
   // ADD-BOX
   const box = document.createElement('div');
   box.classList.add('box');
-  
-  box.append(i); 
-  containerBox.append(box);
 
   // divisibile prima per 3 e poi per 5
-  if (!(i % 3)){
+  if (i % 3 === 0 && i % 5 === 0){
+    box.classList.add('fizz-buzz')
+    box.textContent = ('Buzz-Fizz')
+  } else if (i % 3 === 0){
     box.classList.add('fizz')
     box.textContent = ('Fizz')
-  } else if (!(i % 5)){
+  } else if (i % 5 === 0){
     box.classList.add('buzz')
     box.textContent = ('Buzz')
   }
-
-  // divisibileper 3 e per 5
-  if(!(i % 3) && !(i % 5)){
-   box.classList.add('fizz-buzz')
-   box.textContent = ('Buzz-Fizz')
- }
+      // divisibileper 3 e per 5
+  
+  box.append(i); 
+  containerBox.append(box);
 }
