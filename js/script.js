a9d6d78de42113a0163e117ff4d5dec3d8024866
Melozzo <carmecamma1995@gmail.com>
2023-09-11 16:31:59 +0200

    [edit]: fizz-buzz color

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ for(let i = 1; i <= 100; i++){
   containerBox.append(box);
 
   if (!(i % 3)){
-    box.classList.add('green')
+    box.classList.add('fizz')
   } else if (!(i % 5)){
-    
+    box.classList.add('buzz')
   }
-
+ if(!(i % 3) && !(i % 5)){
+  box.classList.add('fizz-buzz')
+}
 }
