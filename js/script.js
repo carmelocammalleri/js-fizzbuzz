69b7762e13ddabcfdf508f83c0adab1643d61adc
Melozzo <carmecamma1995@gmail.com>
2023-09-11 16:36:32 +0200

    [fix]: names class and add divisible for 3 & 5

--- js/script.js
+++ js/script.js
@@ -8,12 +8,18 @@ for(let i = 1; i <= 100; i++){
   box.append(i); 
   containerBox.append(box);
 
+  // divisibile prima per 3 e poi per 5
   if (!(i % 3)){
     box.classList.add('fizz')
+    box.textContent = ('Fizz')
   } else if (!(i % 5)){
     box.classList.add('buzz')
+    box.textContent = ('Buzz')
   }
- if(!(i % 3) && !(i % 5)){
-  box.classList.add('fizz-buzz')
-}
+
+// divisibileper 3 e per 5
+  if(!(i % 3) && !(i % 5)){
+   box.classList.add('fizz-buzz')
+   box.textContent = ('Buzz-Fizz')
+ }
 }
