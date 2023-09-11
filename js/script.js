0881cdf6a36e7a97893e6a0265d0a6283034c835
Melozzo <carmecamma1995@gmail.com>
2023-09-11 16:17:11 +0200

    [edit]: color green in css and if in js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const containerBox = document.querySelector('.boxes');
 
-for(let i = 0; i < 100; i++){
+for(let i = 1; i <= 100; i++){
   // ADD-BOX
   const box = document.createElement('div');
   box.classList.add('box');
   
   box.append(i); 
   containerBox.append(box);
+
+  if (!(i % 3)){
+    box.classList.add('green')
+  } else if (!(i % 5)){
+    
+  }
+
 }
