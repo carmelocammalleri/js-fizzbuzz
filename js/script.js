3774e34319cc2b492eed1b790db2365e6a13bfd6
Melozzo <carmecamma1995@gmail.com>
2023-09-11 16:05:45 +0200

    [add]: display flex to box

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const containerBox = document.querySelector('.boxes');
 
 for(let i = 0; i < 100; i++){
+  // ADD-BOX
   const box = document.createElement('div');
   box.classList.add('box');
-  box.append(i + 1);
-
   
+  box.append(i); 
   containerBox.append(box);
 }
